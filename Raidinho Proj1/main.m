20394958bcd82aa6d39b4aa963110bec6f2d85d8
Leonardo de Sousa Mendes <lleo.sm@gmail.com>
2014-03-26 14:29:25 -0300

    Initial Commit

---
+++ Raidinho Proj1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Raidinho Proj1
+//
+//  Created by LEONARDO DE SOUSA MENDES on 26/03/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Raidinho Proj1Tests/Raidinho_Proj1Tests.m
@@ -0,0 +1,34 @@
+//
+//  Raidinho_Proj1Tests.m
+//  Raidinho Proj1Tests
+//
+//  Created by LEONARDO DE SOUSA MENDES on 26/03/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Raidinho_Proj1Tests : XCTestCase
+
+@end
+
+@implementation Raidinho_Proj1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
