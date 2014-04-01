d12922a572c612e8ca388311b115f448dcfc9ce3
Julia <julia.a.c.yamamoto@gmail.com>
2014-04-01 15:33:18 -0300

    adicionadas duas novas imagens e conectadas as views no storyboard
    imagens: seta para a direita, seta para a esquerda.

---
+++ Raidinho Proj1/PrincipalViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrincipalViewController.h
+//  Raidinho Proj1
+//
+//  Created by JULIA ARISSA COPPOLA YAMAMOTO on 01/04/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrincipalViewController : UIViewController
+
+@end
